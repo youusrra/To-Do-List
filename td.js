5274b1073724c114cbc35b56c9d1852ac57b25de
youusrra <168985963+youusrra@users.noreply.github.com>
2024-08-08 09:41:59 +0300

    Add files via upload

---
+++ td.js
@@ -0,0 +1,41 @@
+let inputBox = document.getElementById("input-box");
+let listConatiner = document.getElementById("list-container");
+let button = document.querySelector("button");
+
+button.onclick = function addTask() {
+  if (inputBox.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listConatiner.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+};
+
+listConatiner.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listConatiner.innerHTML);
+}
+
+function showTask() {
+  listConatiner.innerHTML = localStorage.getItem("data)");
+}
+showTask();
